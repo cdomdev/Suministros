1c1501710169fd22971c5f1bb05fe2fe5abfb3b7
cdomdev <dominguez5493.cd@gmail.com>
2024-03-08 14:34:12 -0500

    update funtions mails

--- Api/src/modules/controllers/user/controller.js
+++ Api/src/modules/controllers/user/controller.js
@@ -1,6 +1,5 @@
-const path = require("path");
-const fs = require("fs");
 const Sequelize = require("sequelize");
+const sequelize = require("../../../database/conecction");
 const {
   Productos,
   Inventario,
@@ -15,12 +14,29 @@ const {
   calcularTotal,
   subTotal,
 } = require("../../utils/valoresDeProductos");
-const { sendEmailCompra } = require("../../middleware/Mails");
-const sequelize = require("../../../database/conecction");
 const { userExisting } = require("../../middleware/authValidate");
 
 const sendMailsCompra = require('../../../../templates/sendMailsCompra')
 
+
+
+const obtenerDatosUsuario = async (req, res) => {
+  const { email } = req.query;
+
+  console.log(email);
+  try {
+    const dataUser = await User.findAll({ where: { email } });
+    if (!dataUser || dataUser.length === 0) {
+      return res.status(400).json({ message: "No existen datos del usuario" });
+    }
+
+    const { name, picture } = dataUser[0];
+    res.status(200).json({ name, picture });
+  } catch (error) {
+    console.log("Error interno del servidor", error);
+    res.status(500).json({ message: "Error interno en el servidor" });
+  }
+};
 
 const listarProductos = async (req, res) => {
   try {
@@ -266,9 +282,13 @@ const finalizarCompraUsuario = async (req, res) => {
       });
     }
 
+    // for(const producto of dataProducts){
+    //   console.log(producto)
+    // }
+
     console.log(nuevoPedido)
     // Enviar correo electrónico usuario
-    sendMailsCompra(0, user.name, user.email, nuevoPedido.nombre)
+    sendMailsCompra(0, user.name, user.email, nuevoPedido, dataProducts)
 
     await t.commit();
 
@@ -280,59 +300,6 @@ const finalizarCompraUsuario = async (req, res) => {
   }
 };
 
-const construirContenidoCorreo = (user, dataProducts) => {
-  // Leer el contenido de la plantilla de correo electrónico
-  const plantillaPath = path.join(__dirname, "../../../templates/compra.html");
-  const contenidoPlantilla = fs.readFileSync(plantillaPath, "utf-8");
-
-  // Inicializar el contenido del correo con la plantilla base
-  const contenidoCorreo = contenidoPlantilla
-    .replace("{{nombre_usuario}}", user.nombre)
-    .replace("{{detalles_productos}}", productosHTML)
-    .replace("{{precio_total}}", precioTotal);
-
-  // Inicializar variables para contener los detalles de la compra
-  let productosHTML = "";
-  let precioTotal = 0;
-
-  // Iterar sobre cada producto y construir la sección de productos en HTML
-  for (const producto of dataProducts) {
-    productosHTML += `
-    <li>Producto: ${producto.nombre}</li>
-    <li>Precio: ${producto.valor}</li>
-  `;
-    precioTotal += parseFloat(producto.valor * producto.cantidad);
-  }
-
-  // Reemplazar la sección de productos en la plantilla con los detalles de la compra
-  contenidoCorreo = contenidoCorreo.replace(
-    "{{detalles_productos}}",
-    productosHTML
-  );
-
-  // Reemplazar el marcador de posición del precio total
-  contenidoCorreo = contenidoCorreo.replace("{{precio_total}}", precioTotal);
-
-  return contenidoCorreo;
-};
-
-const obtenerDatosUsuario = async (req, res) => {
-  const { email } = req.query;
-
-  console.log(email);
-  try {
-    const dataUser = await User.findAll({ where: { email } });
-    if (!dataUser || dataUser.length === 0) {
-      return res.status(400).json({ message: "No existen datos del usuario" });
-    }
-
-    const { name, picture } = dataUser[0];
-    res.status(200).json({ name, picture });
-  } catch (error) {
-    console.log("Error interno del servidor", error);
-    res.status(500).json({ message: "Error interno en el servidor" });
-  }
-};
 
 const actulizarDatosDeUsuario = async (req, res) => {
   const { email, dataUpdate } = req.body;
@@ -428,7 +395,7 @@ module.exports = {
   buscarProductos,
   finalizarCompraInvitado,
   finalizarCompraUsuario,
-  obtenerDatosUsuario,
   actulizarDatosDeUsuario,
   listarPedidos,
+  obtenerDatosUsuario,
 };
